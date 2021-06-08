cd6e0494cada5ef2588eda11e78a7ec010df69fc
unknown <sshann30@gmail.com>
2021-06-07 21:59:58 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@material-ui/core';
+
+
+const App = () => {
+    return (
+        <div>
+            <Typography variant="h1">Steve is a massive idiot</Typography>
+        </div>
+    );
+}
+
+export default App;
